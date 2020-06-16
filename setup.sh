10226ba98e7912768fa747e70884445fd3ecf0c3
Modulo <modzero@protonmail.com>
2020-06-16 11:29:42 -0500

    remove db

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pm2 delete hello
+git clone https://github.com/modular/node
+cd node
+echo "{\"port\":3000,\"endpoint\":\"https://dev1.modularseed.xyz\",\"modspace\":\"0%1\"}" > config.json
+npm i
+pm2 start index.js
+pm2 save

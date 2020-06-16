ea5d48a8c4bae07e615acf11bc2f0a9005fc714d
Modulo <modzero@protonmail.com>
2020-06-16 12:09:59 -0500

    peers

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 pm2 delete hello
 git clone https://github.com/modular/node
 cd node
-echo "{\"port\":3000,\"endpoint\":\"https://dev1.modularseed.xyz\",\"modspace\":\"0%1\"}" > config.json
+echo "{\"port\":3000,\"endpoint\":\"https://dev.modularseed.xyz\",\"modspace\":\"0%1\"}" > config.json
 npm i
 pm2 start index.js
 pm2 save
